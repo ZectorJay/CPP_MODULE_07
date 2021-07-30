b7b3589fb4e51c7421f1cc1cf418f007bacc32a2
ZectorJay <zKimiko@yandex.ru>
2021-07-30 11:13:19 +0300

    ex00 done

---
+++ ex00/main.cpp
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmickey <hmickey@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/22 21:41:36 by hmickey           #+#    #+#             */
+/*   Updated: 2021/07/30 11:12:04 by hmickey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+void test( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+}
+
+int main(){
+
+	test();
+
+	Awesome u(5);
+	Awesome s(7);
+
+	swap(u, s);
+	std::cout<<u.get_n()<<std::endl;
+}
---
+++ ex00/whatever.hpp
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hmickey <hmickey@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/07/22 21:40:41 by hmickey           #+#    #+#             */
+/*   Updated: 2021/07/30 10:25:53 by hmickey          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef WHATEVER_HPP
+# define WHATEVER_HPP
+# include <iostream>
+
+class Awesome {
+
+public:
+
+	Awesome (int n) : _n(n){}
+	bool operator==(Awesome const & rhs) const { return (this->_n == rhs._n); }
+	bool operator!=(Awesome const & rhs) const { return (this->_n != rhs._n); }
+	bool operator>(Awesome const & rhs) const { return (this->_n > rhs._n); }
+	bool operator<(Awesome const & rhs) const { return (this->_n < rhs._n); }
+	bool operator>=(Awesome const & rhs) const { return (this->_n >= rhs._n); }
+	bool operator<=(Awesome const & rhs) const { return (this->_n <= rhs._n); }
+	int get_n() const { return (_n); }
+
+private:
+
+	int _n;
+};
+
+template <typename T>
+T const & max(T const & value1, T const & value2){
+	return (value1 > value2 ? value1 : value2);
+}
+
+template <typename T>
+T min(T const & value1, T const & value2){
+	return (value1 < value2 ? value1 : value2);
+}
+
+template <typename T>
+void swap(T & value1, T & value2){
+	T value3 = value1;
+	value1 = value2;
+	value2 = value3;
+}
+
+#endif
